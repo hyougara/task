9d89b61e559be2a66d022e455eafbad1e993018d
前田優樹 <orzazous2@gmail.com>
2018-11-01 01:18:11 +0900

    validationの日本語化、name&emailの存在性と最大文字数制限 has_secure_password追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+    if @user.save
+      redirect_to user
+    else
+      render "new"
+    end
   end
 
   def update
@@ -40,6 +44,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end

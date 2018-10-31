dcd477a09b5340442c05aabe5cc824b23ad78688
前田優樹 <orzazous2@gmail.com>
2018-11-01 00:41:41 +0900

    部分テンプレート作成、(header,footer,error_messages,rails_default) bootstrap適用、デバッグ表示、DB:user追加、full_title追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # root to: "homes#top"
-  # get "/signup", to: "users#new"
-  # get "/login", to: "sessions#new"
-  # post "/login", to: "sessions#create"
-  # delete "/logout", to: "sessions#destroy"
+  root to: "homes#top"
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

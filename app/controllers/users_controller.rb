0efee29f97efe803510b1bd8b857399412a25228
前田優樹 <orzazous2@gmail.com>
2018-11-01 22:11:11 +0900

    default_user_image追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user
+      redirect_to @user
     else
       render "new"
     end
@@ -44,6 +44,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation,:image)
     end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+    # def default_image
+    #     img_tag(default.png,alt: default_image,class:"default-image")
+    # end
 end

08825d973a741126a1473a80d5cd36491adc39b7
前田優樹 <orzazous2@gmail.com>
2018-11-04 23:56:22 +0900

    投稿を降順に変更

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,13 @@
 class Micropost < ApplicationRecord
     validates :title, presence: true, length:{maximum:50}
     validates :content,presence:true, length:{maximum:255}
+    # カテゴリに属している
     belongs_to :category
+    # enumで状態を定義する(未着手、着手、完了)
     enum status:{draft: 0 ,woking: 1 ,pulished: 2}
-
+    # デフォルトで投稿が降順になる、全てのクエリに適用
+    default_scope -> {order(created_at: :desc) }    
+            
     # class << self
     #     def localed_statuses
     #         statuses.keys.map do |s|
